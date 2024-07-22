913151baa1c9e805d61dd9ad0ab4238aca7b3841
Ahsan khalid <ahsankhalid816@gmail.com>
2024-07-22 11:30:08 +0500

    pass stage

--- app/main.js
+++ app/main.js
@@ -10,9 +10,9 @@ const server = net.createServer((socket) => {
     const request = data.toString();
 
     if (request.startsWith("GET / ")) {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      console.log(socket.write("HTTP/1.1 200 OK\r\n\r\n"));
     } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      console.log(socket.write("HTTP/1.1 404 Not Found\r\n\r\n"));
     }
     socket.end();
   });
