15ade7e32ee2219432e8576952d43c5c8e19ddf0
Ahsan khalid <ahsankhalid816@gmail.com>
2024-08-02 17:49:49 +0500

    done

--- app/main.js
+++ app/main.js
@@ -5,15 +5,23 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
+    const lines = request.split("\r\n");
+    const requestLine = lines[0]; // e.g., GET /echo/abc HTTP/1.1
 
-    if (request.startsWith("GET / ")) {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (request.startsWith("GET /echo/")) {
-      const content = request.split("GET /echo/")[1];
-      const contentLength = Buffer.byteLength(content, "utf8"); // Accurate byte length of content
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n${content}`
-      );
+    if (requestLine.startsWith("GET /echo/")) {
+      // Extract the string from the URL
+      const content = requestLine.split("GET /echo/")[1].split(" ")[0];
+      const contentLength = Buffer.byteLength(content, 'utf8');
+
+      // Construct the HTTP response
+      const response = 
+        `HTTP/1.1 200 OK\r\n` +
+        `Content-Type: text/plain\r\n` +
+        `Content-Length: ${contentLength}\r\n` +
+        `\r\n` +  // End of headers
+        `${content}`;  // Response body
+
+      socket.write(response);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
@@ -27,11 +35,6 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     console.log("Socket closed");
   });
-});
-
-
-server.on("close", () => {
-  console.log("Server closed");
 });
 
 server.listen(4221, "localhost", () => {
