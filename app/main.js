138bce34aedcc89d9b7fdafc971b611d781bbdaa
Ahsan khalid <ahsankhalid816@gmail.com>
2024-08-02 17:35:47 +0500

    Completed

--- app/main.js
+++ app/main.js
@@ -2,24 +2,38 @@ const net = require("net");
 
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  //   socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("data", (data) => {
     const request = data.toString();
 
     if (request.startsWith("GET / ")) {
-      console.log(socket.write("HTTP/1.1 200 OK\r\n\r\n"));
-    } else if (request.startsWith("GET /echo /")) {
-      const content = request.split("GET /echo /")[1];
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (request.startsWith("GET /echo/")) {
+      const content = request.split("GET /echo/")[1].trim();
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
       );
     } else {
-      console.log(socket.write("HTTP/1.1 404 Not Found\r\n\r\n"));
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
+  });
+
+  socket.on("error", (e) => {
+    console.error("ERROR: " + e);
     socket.end();
+  });
+
+  // Handle socket close event
+  socket.on("close", () => {
+    console.log("Socket closed");
   });
 });
 
-server.listen(4221, "localhost");
+// Handle server close event
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(4221, "localhost", () => {
+  console.log("Server listening on port 4221");
+});
